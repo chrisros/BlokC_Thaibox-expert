fe6359c5003ceedb75e749c696e7de9bc83f596e
Chris Ros <chris.ros@mylcros.nl>
2015-03-19 00:11:40 +0100

    update 1
    toevoeging 'over ons' (niet final)
    toevoeging placeholder algemene voorwaarden
    verbetering navbar
    toevoeging  geldcounter (werkt niet)
    algemene GUI verbeteringen/ bugfixes

---
+++ Webshop2/Webshop2/Scripts/dropdown.js
@@ -0,0 +1,12 @@
+﻿function menuOpen() {
+    
+    document.getElementById('dropMenu').style.display = 'block';
+}
+
+document.getElementById('dropMenu').onmouseout = function (e) {
+    e = e || window.event;
+    var target = e.srcElement || e.target;
+    if (target.id == "dropMenu") {
+        document.getElementById('dropMenu').style.display = 'none';
+    }
+};
