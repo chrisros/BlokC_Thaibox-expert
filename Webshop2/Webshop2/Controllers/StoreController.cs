6481e941f09baa27329112c8342fe8504366b70e
aaikoosters <aaik.oosters@gmail.com>
2015-03-19 15:18:48 +0100

    Statische product pagina
    Staan 6 producten naast elkaar en onderelkaar:
    1 | 1 | 1
    1 | 1 | 1
    
    Zijn door klik baar, code daar achter werkt nog niet.
    En ik heb wat test afbeeldingen toegevoegd

--- Webshop2/Webshop2/Controllers/StoreController.cs
+++ Webshop2/Webshop2/Controllers/StoreController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Webshop2.Models;
 
 namespace Webshop2.Controllers
 {
@@ -14,6 +15,33 @@ namespace Webshop2.Controllers
             ViewBag.H1 = "Winkel";
 
             return View();
+        }
+
+        public ActionResult ProductDetail(int productID)
+        {
+            ViewBag.H1 = "Product Detail";
+
+            Models.Product p = new Models.Product();
+
+            if (productID == 1)
+            {
+                p.productID = productID;
+                p.productNaam = "Sport broek";
+                p.productDetail = "Strakke sport broek met wat leuke kleurtjes";
+            }
+            else if (productID == 2)
+            {
+                p.productID = productID;
+                p.productNaam = "Hesje";
+                p.productDetail = "Geel hesje met reflectie materiaal";
+            }
+            else
+            {
+                p.productNaam = "Onbekend";
+                p.productDetail = "Onbekend";
+            }
+
+            return View(p);
         }
     }
 }
---
+++ Webshop2/Webshop2/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Webshop2.Models
+{
+    public class Product
+    {
+        public String productNaam { get; set; }
+        public String productDetail { get; set; }
+        public int productID { get; set; }
+    }
+}
