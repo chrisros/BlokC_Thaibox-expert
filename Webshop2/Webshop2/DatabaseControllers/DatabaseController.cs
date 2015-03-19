56c0b2f9c10849bc2d4f734c4fc4285594f97a59
Wahad <rcvanderende@gmail.com>
2015-03-19 15:13:25 +0100

    Account registratie begin
    Komt nog niet in de database, methodes staan er.

---
+++ Webshop2/Webshop2/DatabaseControllers/DatabaseController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MySql.Data.MySqlClient;
+
+namespace Webshop2.DatabaseControllers
+{
+    public abstract class DatabaseController
+    {
+        protected MySqlConnection conn;
+
+        public DatabaseController()
+        {
+            //Vul hier de juiste gegevens in!!
+            conn = new MySqlConnection("Server=meru.hhs.nl;Database=14108011;Uid=14108011;Pwd=iNucairahf;");
+        }
+    }
+}
--- Webshop2/Webshop2/Models/AccountModel.cs
+++ Webshop2/Webshop2/Models/AccountModel.cs
@@ -7,5 +7,15 @@ namespace Webshop2.Models
 {
     public class AccountModel
     {
+        public String Naam { get; set; }
+        public String Woonadres { get; set; }
+        public String Woonpostcode { get; set; }
+        public String Gebruikersnaam { get; set; }
+        public String Wachtwoord { get; set; }
+        public String Email { get; set; }
+        public DateTime GeboorteDatum { get; set; }
+        public Int32 Telefoonnummer { get; set; }
+        public String Leveradres { get; set; }
+        public String Leverpostcode { get; set; }
     }
 }
