88e09544266f4cfed2ea5f6859258ba0afa50d07
Chris Ros <chris.ros@mylcros.nl>
2015-03-23 19:02:44 +0100

    Mailing contact
    Contact form (mailform) geintregeerd
    class voor mailing is er nu ook (basis voor alle andere mailfeatures)

--- Webshop2/Webshop2/Controllers/AboutController.cs
+++ Webshop2/Webshop2/Controllers/AboutController.cs
@@ -8,11 +8,14 @@ namespace Webshop2.Controllers
 {
     public class AboutController : Controller
     {
+       
         // GET: About
         public ActionResult Index()
         {
             ViewBag.H1 = "Over ons";
             return View();
         }
+        
+
     }
 }
--- Webshop2/Webshop2/Controllers/ContactController.cs
+++ Webshop2/Webshop2/Controllers/ContactController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Webshop2.mailControllers;
 
 namespace Webshop2.Controllers
 {
@@ -12,6 +13,15 @@ namespace Webshop2.Controllers
         public ActionResult Index()
         {
             ViewBag.H1 = "Contact";
+            return View();
+        }
+       [HttpPost]
+        public ActionResult Mailsend(FormCollection formCollection)
+        {
+            MailSendController email = new MailSendController { EmailAdresNaar = "thaiboxexpert@chros.nl", Onderwerp = formCollection["subject"], Bericht = formCollection["message"], emailAdreszender = formCollection["email"], Naam = formCollection["name"] };
+            email.SendEmail();
+            ViewBag.H1 = "Over ons";
+            ViewBag.feedback = "Mail verstuurd, u hoort zo spoedig mogelijk van ons.";
             return View();
         }
     }
